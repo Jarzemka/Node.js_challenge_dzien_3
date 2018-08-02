f7539f904cebd8e5bb7ea62959847cd18046853b
Malgorzata <jarzemska.malgorzata@gmail.com>
2018-08-02 22:26:07 +0200

    challange 3

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,3 +1,26 @@
+const crypto = require('crypto');
+
 const MY_PWD_HASH = '5dca0fc4e306d92b2077ad85e7c4bd87a3e8648e';
 
-//Twój kod
+const passwords = [
+	'??TegoHasła',
+	'CodersLab',
+	'Node.js Szyfruje Pliki',
+	'Zaźółć Gęślą Jaźń',
+	'Moje Haslo 1@3!',
+	'111#$((@)n',
+	'Dzisiaj Szyfruje 83'
+];
+const hashFuncs = ['sha256', 'sha512', 'rmd160', 'md5'];
+
+passwords.forEach(password => {
+	hashFuncs.forEach(hashFunc => {
+    const hash = crypto.createHmac(hashFunc, password).digest('hex');
+    if (hash == MY_PWD_HASH) {
+      console.log(password, hashFunc, hash);
+    }
+	});
+});
+
+
+
--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,10 @@
-//Twój kod
+const fs = require('fs');
+const crypto = require('crypto');
+
+fs.readFile(testFile,'utf8', (err, data) => {
+    if (err === null){
+        console.log(data)
+    }else{
+         console.log('błąd odczytu', err);
+    }
+    });
